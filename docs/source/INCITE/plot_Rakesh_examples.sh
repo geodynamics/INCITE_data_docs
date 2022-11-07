97bc429d5cfdc4bcffdf8c957fac51c989ec3c64
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2022-11-06 21:35:36 -0800

    Add shell script to make all images

---
+++ docs/source/INCITE/plot_Rakesh_examples.sh
@@ -0,0 +1,12 @@
+# /bin/sh
+
+python3 INCITE_convert_plot/plot_Rakesh_examples.py 
+"ryadav/ensemble2/Pm0.05_Pr1_eta0.35_Ek1E-6_Ra2.5E10_no-slip_dyn" \
+"ryadav/ensemble2/Pm0.1_Pr1_eta0.35_Ek1E-6_Ra1E10_no-slip_dyn" \
+"ryadav/ensemble2/Pm0.2_Pr1_eta0.35_Ek1E-6_Ra5E9_no-slip_dyn" \
+"ryadav/ensemble3/Pm0.05_Pr0.1_eta0.35_Ek1E-6_Ra2.5E9_no-slip_dyn" \
+"ryadav/ensemble3/Pm0.5_Pr1_eta0.35_Ek1E-6_Ra3E9_no-slip_dyn" \
+"ryadav/ensemble3/Pm1_Pr1_eta0.35_Ek1E-6_Ra3E9_no-slip_dyn" \
+"ryadav/ensemble4/Pm0.03_Pr1_eta0.35_Ek1E-7_Ra3E11_no-slip_dyn" \
+"ryadav/ensemble4/Pm0.05_Pr1_eta0.35_Ek1E-7_Ra1.5E11_no-slip_dyn" \
+"ryadav/ensemble4/Pm0.2_Pr1_eta0.35_Ek1E-7_Ra3E10_no-slip_dyn"
