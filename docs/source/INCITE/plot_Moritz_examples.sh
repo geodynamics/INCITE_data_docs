7068a14baa7b18ac1bb4b33c11a8a58082506c39
Hiroaki Matsui <matsui@Kemo-mac-2009.local>
2022-11-08 09:14:49 -0800

    Update images

--- docs/source/INCITE/plot_Moritz_examples.sh
+++ docs/source/INCITE/plot_Moritz_examples.sh
@@ -16,101 +16,111 @@
 #    "mheimpel/JupNatGeo/E3n6/run5/" \
 #    "mheimpel/JupNatGeo/E3n6/run6/" \
 #
+#  "mheimpel/Jup90/fsb/E3n6/run1/" \
+#  "mheimpel/Jup90/fsb/E3n6/runM1/" \
+#  "mheimpel/Jup90/fsb/E3n6/runM2/" \
+#  "mheimpel/Jup90/fsb/E3n6/runM3/" \
+#  "mheimpel/Jup90/fsb/E3n6/runM4/" \
+#  "mheimpel/Jup90/fsb/E3n6/runM5/" \
+#    "mheimpel/Jup90/fsb/E3n6/runM6/" \
+#
+#   "mheimpel/Jup95/fsb/poly_n1/E1n5/run1a/" \
+#  "mheimpel/Jup95/fsb/poly_n1/E1n5/run1b/" \
+#  "mheimpel/Jup95/fsb/poly_n1/E1n5/run1c/" \
+#  "mheimpel/Jup95/fsb/poly_n1/E1n5/run2/" \
+#  "mheimpel/Jup95/fsb/poly_n1/E1n5/run3/" \
+#  "mheimpel/Jup95/fsb/poly_n1/E1n5/run4/" \
+#  "mheimpel/Jup95/fsb/poly_n1/E1n5/run5/" \
+#  "mheimpel/Jup95/fsb/poly_n1/E1n5/run6/" \
+#  "mheimpel/Jup95/fsb/poly_n1/E1n5/run7/" \
+#  "mheimpel/Jup95/fsb/poly_n1/E3n5/run1a/" \
+#  "mheimpel/Jup95/fsb/poly_n1/E3n5/run1b/" \
+#  "mheimpel/Jup95/fsb/poly_n1/E3n5/run1c/" \
+#  "mheimpel/Jup95/fsb/poly_n1/E3n6/run1a/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E1n5/run1/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E1n5/run2/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E1n5/run3/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E1n5/run4/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E1n6/run1a/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E1n6/run1b/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E1n6/run1c/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E1n6/run1d/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E1n6/run2/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E1n6/run3/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E1n6/run4/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E1n6/runM1/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E1n6/runM2/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E1n6/runM3/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E1n6/runM4/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E1n6/runM5/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E1n6/runM6/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E1n6/runM7/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E2n6/run1/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E3n5/run1/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E3n5/run2/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E3n5/run3/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E3n5/run4/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E3n5/run5/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E3n5/run6/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E3n5/run7/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E3n5/run8/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E3n6/run1/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E3n6/run2/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E3n6/run3/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E3n6/run4/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E3n6/run5/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E3n6/run6/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E3n6/run7/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E3n6/run8/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E3n6/runM1/" \
+#  "mheimpel/Jup95/nsb/poly_n1/E3n7/run1a/" \
+#  "mheimpel/Jup95/nsb/poly_n2/E1n5/run1/" \
+#  "mheimpel/Jup95/nsb/poly_n2/E1n5/run2/" \
+#  "mheimpel/Jup95/nsb/poly_n2/E3n5/run1/" \
+#
+#
+#
+#  Almost no data
+#    "mheimpel/Jup90/fsb/E3n6/run2/" \
+#    "mheimpel/Jup90/fsb/E3n6/run3/" \
+#    "mheimpel/Jup90/nsb/E3n6/run1/" \
+#    "mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n6/run01/" \
+#    "mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n6/run02/" \
+#    "mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n6/runR1/" \
+#    "mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n6/runR2/" \
+#    "mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n6/runR3/" \
+#    "mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n6/runR4/" \
+#    "mheimpel/Jup95/fsb/poly_n2/heating_type1/E3n5/run3/" \
+#
+#  Done
+#    "mheimpel/Jup90/fsb/E3n6/run1/" \
+#  "mheimpel/Jup90/nsb/E1n4/run1/" \
+#  "mheimpel/Jup90/nsb/E1n4/run2/" \
+#  "mheimpel/Jup90/nsb/E1n4/run3/" \
+#  "mheimpel/Jup90/nsb/E1n5/run1/" \
+#  "mheimpel/Jup90/nsb/E1n5/run2/" \
+#  "mheimpel/Jup90/nsb/E3n5/run1/" \
+#  "mheimpel/Jup90/nsb/E3n5/run2/" \
+#  "mheimpel/Jup90/nsb/E3n5/run3/" \
+#
+#  "mheimpel/Jup95/fsb/poly_n1/E1n4/run1/" \
+#  "mheimpel/Jup95/fsb/poly_n1/E1n4/run2/" \
+# "mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n4/run1/" \
+# "mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n4/run2/" \
+# "mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n4/run3/" \
+# "mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n5/run01/" \
+# "mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n5/run02/" \
+# "mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n5/run03/" \
+# "mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n5/run04/" \
+# "mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n5/run1/" \
+# "mheimpel/Jup95/fsb/poly_n2/heating_type1/E3n4/run1/" \
+# "mheimpel/Jup95/fsb/poly_n2/heating_type1/E3n4/run2/" \
+# "mheimpel/Jup95/fsb/poly_n2/heating_type1/E3n4/run3/" \
+# "mheimpel/Jup95/fsb/poly_n2/heating_type1/E3n5/run1/" \
+# "mheimpel/Jup95/fsb/poly_n2/heating_type1/E3n5/run2/" \
+#
 
 python3 INCITE_convert_plot/plot_Moritz_examples.py \
-"mheimpel/Jup90/fsb/E3n6/run1/" \
-"mheimpel/Jup90/fsb/E3n6/run2/" \
-"mheimpel/Jup90/fsb/E3n6/run3/" \
-"mheimpel/Jup90/fsb/E3n6/runM1/" \
-"mheimpel/Jup90/fsb/E3n6/runM2/" \
-"mheimpel/Jup90/fsb/E3n6/runM3/" \
-"mheimpel/Jup90/fsb/E3n6/runM4/" \
-"mheimpel/Jup90/fsb/E3n6/runM5/" \
-"mheimpel/Jup90/fsb/E3n6/runM6/" \
-"mheimpel/Jup90/nsb/E1n4/run1/" \
-"mheimpel/Jup90/nsb/E1n4/run2/" \
-"mheimpel/Jup90/nsb/E1n4/run3/" \
-"mheimpel/Jup90/nsb/E1n5/run1/" \
-"mheimpel/Jup90/nsb/E1n5/run2/" \
-"mheimpel/Jup90/nsb/E3n5/run1/" \
-"mheimpel/Jup90/nsb/E3n5/run2/" \
-"mheimpel/Jup90/nsb/E3n5/run3/" \
-"mheimpel/Jup90/nsb/E3n6/run1/" \
-"mheimpel/Jup95/fsb/poly_n1/E1n4/run1/" \
-"mheimpel/Jup95/fsb/poly_n1/E1n4/run2/" \
-"mheimpel/Jup95/fsb/poly_n1/E1n5/run1a/" \
-"mheimpel/Jup95/fsb/poly_n1/E1n5/run1b/" \
-"mheimpel/Jup95/fsb/poly_n1/E1n5/run1c/" \
-"mheimpel/Jup95/fsb/poly_n1/E1n5/run2/" \
-"mheimpel/Jup95/fsb/poly_n1/E1n5/run3/" \
-"mheimpel/Jup95/fsb/poly_n1/E1n5/run4/" \
-"mheimpel/Jup95/fsb/poly_n1/E1n5/run5/" \
-"mheimpel/Jup95/fsb/poly_n1/E1n5/run6/" \
-"mheimpel/Jup95/fsb/poly_n1/E1n5/run7/" \
-"mheimpel/Jup95/fsb/poly_n1/E3n5/run1a/" \
-"mheimpel/Jup95/fsb/poly_n1/E3n5/run1b/" \
-"mheimpel/Jup95/fsb/poly_n1/E3n5/run1c/" \
-"mheimpel/Jup95/fsb/poly_n1/E3n6/run1a/" \
-"mheimpel/Jup95/nsb/poly_n1/E1n5/run1/" \
-"mheimpel/Jup95/nsb/poly_n1/E1n5/run2/" \
-"mheimpel/Jup95/nsb/poly_n1/E1n5/run3/" \
-"mheimpel/Jup95/nsb/poly_n1/E1n5/run4/" \
-"mheimpel/Jup95/nsb/poly_n1/E1n6/run1a/" \
-"mheimpel/Jup95/nsb/poly_n1/E1n6/run1b/" \
-"mheimpel/Jup95/nsb/poly_n1/E1n6/run1c/" \
-"mheimpel/Jup95/nsb/poly_n1/E1n6/run1d/" \
-"mheimpel/Jup95/nsb/poly_n1/E1n6/run2/" \
-"mheimpel/Jup95/nsb/poly_n1/E1n6/run3/" \
-"mheimpel/Jup95/nsb/poly_n1/E1n6/run4/" \
-"mheimpel/Jup95/nsb/poly_n1/E1n6/runM1/" \
-"mheimpel/Jup95/nsb/poly_n1/E1n6/runM2/" \
-"mheimpel/Jup95/nsb/poly_n1/E1n6/runM3/" \
-"mheimpel/Jup95/nsb/poly_n1/E1n6/runM4/" \
-"mheimpel/Jup95/nsb/poly_n1/E1n6/runM5/" \
-"mheimpel/Jup95/nsb/poly_n1/E1n6/runM6/" \
-"mheimpel/Jup95/nsb/poly_n1/E1n6/runM7/" \
-"mheimpel/Jup95/nsb/poly_n1/E2n6/run1/" \
-"mheimpel/Jup95/nsb/poly_n1/E3n5/run1/" \
-"mheimpel/Jup95/nsb/poly_n1/E3n5/run2/" \
-"mheimpel/Jup95/nsb/poly_n1/E3n5/run3/" \
-"mheimpel/Jup95/nsb/poly_n1/E3n5/run4/" \
-"mheimpel/Jup95/nsb/poly_n1/E3n5/run5/" \
-"mheimpel/Jup95/nsb/poly_n1/E3n5/run6/" \
-"mheimpel/Jup95/nsb/poly_n1/E3n5/run7/" \
-"mheimpel/Jup95/nsb/poly_n1/E3n5/run8/" \
-"mheimpel/Jup95/nsb/poly_n1/E3n6/run1/" \
-"mheimpel/Jup95/nsb/poly_n1/E3n6/run2/" \
-"mheimpel/Jup95/nsb/poly_n1/E3n6/run3/" \
-"mheimpel/Jup95/nsb/poly_n1/E3n6/run4/" \
-"mheimpel/Jup95/nsb/poly_n1/E3n6/run5/" \
-"mheimpel/Jup95/nsb/poly_n1/E3n6/run6/" \
-"mheimpel/Jup95/nsb/poly_n1/E3n6/run7/" \
-"mheimpel/Jup95/nsb/poly_n1/E3n6/run8/" \
-"mheimpel/Jup95/nsb/poly_n1/E3n6/runM1/" \
-"mheimpel/Jup95/nsb/poly_n1/E3n7/run1a/" \
-"mheimpel/Jup95/nsb/poly_n2/E1n5/run1/" \
-"mheimpel/Jup95/nsb/poly_n2/E1n5/run2/" \
-"mheimpel/Jup95/nsb/poly_n2/E3n5/run1/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n4/run1/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n4/run2/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n4/run3/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n5/run01/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n5/run02/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n5/run03/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n5/run04/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n5/run1/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n6/run01/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n6/run02/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n6/runR1/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n6/runR2/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n6/runR3/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E1n6/runR4/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E3n4/run1/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E3n4/run2/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E3n4/run3/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E3n5/run1/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E3n5/run2/" \
-"mheimpel/Jup95/fsb/poly_n2/heating_type1/E3n5/run3/" \
 "mheimpel/Jup95/fsb/poly_n2/heating_type1/E3n5/run4/" \
 "mheimpel/Jup95/fsb/poly_n2/heating_type1/E3n5/run5/" \
 "mheimpel/Jup95/fsb/poly_n2/heating_type1/E3n5/run6/" \
